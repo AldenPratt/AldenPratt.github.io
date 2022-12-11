b4c5ae8602f070f3f0e0e6f46255d514b51db6e3
AldenPratt <50831062+AldenPratt@users.noreply.github.com>
2022-12-11 15:00:59 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -14,7 +14,9 @@ function draw() {
   let r1 = map(mouseX, 0, width, 0, height);
   let r2 = height - r1;
 
+  tint(255, r1);
   image(img1, width / 2 + r1 / 2, height / 2, r1, r1);
 
-  image(img2 / 2 - r2 / 2, height / 2, r2, r2);
+  tint(255, r2);
+  image(img2, width / 2 - r2 / 2, height / 2, r2, r2);
 }
